9b304d96cbed77c0d7fd3235b1c2113dc75bdeac
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-11-02 11:39:40 +0530

    Red heart rain

---
+++ Red heart rain/script.js
@@ -0,0 +1,12 @@
+function createHeart(){
+const heart=document.createElement('div');
+heart.classList.add('heart')
+heart.innerText='❤️';
+// heart.innerText='💧';
+
+heart.style.right=Math.random()*100+'vw';
+heart.style.animationDuration=Math.random()*2+3+'s';
+document.body.appendChild(heart);
+setTimeout(()=>{heart.remove()},4000)
+}
+setInterval(createHeart,300);
