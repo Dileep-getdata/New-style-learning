8ff7f3d939aed4167a132429d81003537eaf35dc
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-11-03 20:26:55 +0530

    Eccomerce webpage just with html and css

--- Hamburger/script.js
+++ Hamburger/script.js
@@ -1,6 +1,24 @@
 const btn=document.getElementById('btn');
+const activate=document.querySelector('.activate');
+console.log(activate)
 const nav=document.getElementById('nav');
 btn.addEventListener('click',()=>{
     nav.classList.toggle('activate');
     btn.classList.toggle('activate');
+    
+
+    
 })
+
+const text=document.getElementById('text');
+const line="Click on more option '☰' to find more intresting features."
+let idx=0;
+function autoText(){
+    idx++;
+    text.innerText=line.slice(0,idx);
+    if(idx>line.length-1){
+        setTimeout(()=>{idx=0;},1000)
+        
+    }
+}
+setInterval(autoText,80);
